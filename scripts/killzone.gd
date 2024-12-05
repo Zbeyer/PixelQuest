b629bd277f5c1cbacc44303794842b955396abf2
Zachary Beyer <zack@boomlearning.com>
2024-12-05 13:04:34 -0700

    more player animations

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,4 +1,12 @@
 extends Area2D
 
 func _on_body_entered(body):
+	#body.get_node("CollisionShape2D").queue_free()
 	print("You Died!")
+	get_tree().reload_current_scene()
+	#Engine.time_scale = 0.5
+	#timer.start()
+
+#func _on_timer_timout():
+	#Engine.time_scale = 1.0
+	#get_tree().reload_current_scene()
